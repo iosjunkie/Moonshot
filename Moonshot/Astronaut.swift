9e6050eac4d13b609497b4e9be245bab61ea3f51
Jules Lee <jackconsolidated@gmail.com>
2020-01-03 04:23:43 +0800

    Finally works

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jules Lee on 1/3/20.
+//  Copyright © 2020 Jules Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Mission.swift
@@ -0,0 +1,37 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Jules Lee on 1/3/20.
+//  Copyright © 2020 Jules Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    let id: Int
+    let launchDate: Date?
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+}
